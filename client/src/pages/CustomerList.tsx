d14d284152d2e874d71927eb42322a891ca48101
Eduardo Gallegos <gallegoseduardo414@gmail.com>
2024-04-26 17:13:29 +0000

    Completed feature 4

--- client/src/pages/CustomerList.tsx
+++ client/src/pages/CustomerList.tsx
@@ -1,14 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './CustomerList.css';
-
-export type Customer = {
-  customerId: number;
-  name: string;
-  address: string;
-  email: string;
-  phoneNumber: string;
-};
+import { type Customer } from '../data';
 
 export function CustomerList() {
   const [customers, setCustomers] = useState<Customer[]>();
@@ -43,22 +36,26 @@ export function CustomerList() {
     );
   }
   return (
-    <div className="container">
-      <h1 className="mb-14 mt-14 customer-list-heading text-black">
-        All Customers
-      </h1>
-      <div className="row">
-        {customers?.map(
-          (
-            customer // build an array of customer cards
-          ) => (
-            <div
-              key={customer.customerId} // React making sure every item it creates has a trackable identifier
-            >
-              <CustomerCard customer={customer} />
-            </div>
-          )
-        )}
+    <div>
+      <div>
+        <h1 className="mb-14 mt-14 customer-list-heading text-black flex justify-center">
+          All Customers
+        </h1>
+      </div>
+      <div className=" flex justify-center">
+        <div className="flex flex-wrap justify-center">
+          {customers?.map(
+            (
+              customer // build an array of customer cards
+            ) => (
+              <div
+                key={customer.customerId} // React making sure every item it creates has a trackable identifier
+                className="basis-2/3 lg:basis-1/2">
+                <CustomerCard customer={customer} />
+              </div>
+            )
+          )}
+        </div>
       </div>
     </div>
   );
@@ -71,26 +68,26 @@ type CustomerProps = {
 function CustomerCard({ customer }: CustomerProps) {
   const { name, phoneNumber, address, email } = customer; // will need  customerId eventually
   return (
-    <div>
-      <div className="flex justify-center">
-        <div className="w-96">
-          <div className="card-body border-solid border-4 border-black mt-4 mb-4 rounded-2xl text-xl bg-white text-black">
-            <p className="mt-4 mb-4">Name: {name}</p>
-            <p className="mt-4 mb-4">Phone Number: {phoneNumber}</p>
-            <p className="mt-4 mb-4">Address: {address}</p>
-            <p className="mt-4 mb-4">Email: {email}</p>
-            <div className="mt-7 mb-7">
-              <Link
-                className="border-solid border-2 border-black p-3 bg-blue-600 text-white rounded-lg text-lg"
-                to={`/customer-form/${customer.customerId}`}>
-                Edit
-              </Link>
-            </div>
-            <div className="mt-7 mb-7">
-              <Link to="/" className="text-lg text-red-500 p-3">
-                Back{' '}
-              </Link>
-            </div>
+    <div className="flex justify-center ">
+      <div className="w-96">
+        <div className="card-body border-solid border-4 border-black mt-4 mb-4 rounded-2xl text-xl bg-white text-black">
+          <p className="mt-4 mb-4">Name: {name}</p>
+          <p className="mt-4 mb-4">Phone Number: {phoneNumber}</p>
+          <p className="mt-4 mb-4">Address: {address}</p>
+          <p className="mt-4 mb-4">Email: {email}</p>
+          <div className="mt-7 mb-7 flex justify-center">
+            <Link
+              className="border-2 border-black bg-blue-600 rounded-2xl text-lg hover:bg-blue-700 text-white hover:text-white w-20 h-8"
+              to={`/customer-form/${customer.customerId}`}>
+              Edit
+            </Link>
+          </div>
+          <div className="mt-7 mb-7">
+            <Link
+              to="/"
+              className="text-lg text-red-500 p-3 hover:text-red-600">
+              Back
+            </Link>
           </div>
         </div>
       </div>
